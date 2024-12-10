4692f7458f561b96edc641c8d3505167b232eae7
Infinite-Learner <msdeepakpious@gmail.com>
2024-12-10 03:06:54 -0800

    new problems

--- LC75/Binary_Search/Successful_Pairs_of_Spells_and_Potions.java
+++ LC75/Binary_Search/Successful_Pairs_of_Spells_and_Potions.java
@@ -5,7 +5,7 @@ public class Successful_Pairs_of_Spells_and_Potions {
     public static int[] successfulPairs(int[] spells, int[] potions, long success) {
         int[] successfulPairs = new int[spells.length];
         int left = 0 , right = potions.length - 1;
-       
+       while
         return successfulPairs;
     }
     public static void main(String[] args) {
